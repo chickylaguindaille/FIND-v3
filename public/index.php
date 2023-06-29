82722518422cd52519b0680b7ea9730262e07dde
chickylaguindaille <91053740+chickylaguindaille@users.noreply.github.com>
2023-06-29 14:03:08 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,16 +1,9 @@
 <?php
 
 use App\Kernel;
-use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-$request = Request::createFromGlobals();
-$request->server->set('BASE', '/FIND-v3/public');
-
-$kernel = new Kernel($request->server->get('APP_ENV', 'dev'), $request->server->get('APP_DEBUG', true));
-
-$response = $kernel->handle($request);
-$response->send();
-
-$kernel->terminate($request, $response);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
