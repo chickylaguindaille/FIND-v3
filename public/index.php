f527c65497cbe12a3c7103b88fa76ad23477f06f
chickylaguindaille <geti.leroy@gmail.com>
2025-02-26 17:35:40 +0100

    add Dockerfile

--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+// Définition de l'environnement et du mode debug
+$env = getenv('APP_ENV') ?: 'dev';
+$debug = (bool) (getenv('APP_DEBUG') ?: ('prod' !== $env));
+
+// En local, charger .env si le fichier existe
+if ($env === 'dev' && file_exists(dirname(__DIR__).'/.env')) {
+    (new Dotenv())->load(dirname(__DIR__).'/.env');
+}
+
+return function (array $context) use ($env, $debug) {
+    return new Kernel($env, $debug);
 };
