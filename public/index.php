871dc83a7a6491080c144d4e32cd7e56e026dc89
chickylaguindaille <geti.leroy@gmail.com>
2025-02-27 00:47:53 +0100

    add Dockerfile

--- public/index.php
+++ public/index.php
@@ -7,12 +7,11 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 // Définition de l'environnement et du mode debug
 $env = getenv('APP_ENV') ?: 'dev';
-exit(var_dump($env));
 $debug = (bool) (getenv('APP_DEBUG') ?: ('prod' !== $env));
 
 // En local, charger .env si le fichier existe
-if ($env === 'dev' && file_exists(dirname(__DIR__).'/.env')) {
-    (new Dotenv())->load(dirname(__DIR__).'/.env');
+if ($env === 'dev' && file_exists(dirname(__DIR__).'/../.env')) {
+    (new Dotenv())->load(dirname(__DIR__).'/../.env');
 }
 
 return function (array $context) use ($env, $debug) {
