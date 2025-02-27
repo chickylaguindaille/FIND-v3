18d29ef4c9ecfda0759a74502187421afbe514ad
chickylaguindaille <geti.leroy@gmail.com>
2025-02-27 01:13:25 +0100

    add Dockerfile

--- public/index.php
+++ public/index.php
@@ -2,18 +2,41 @@
 
 use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-// Définition de l'environnement et du mode debug
-$env = getenv('APP_ENV') ?: 'dev';
-$debug = (bool) (getenv('APP_DEBUG') ?: ('prod' !== $env));
+// Vérifie si on est en production (défini par le serveur)
+$env = getenv('APP_ENV');
 
-// En local, charger .env si le fichier existe
-if ($env === 'dev' && file_exists(dirname(__DIR__).'/../.env')) {
-    (new Dotenv())->load(dirname(__DIR__).'/../.env');
+if (!$env) {
+    // Si aucune variable d'environnement, charge le fichier .env (LOCAL UNIQUEMENT)
+    if (file_exists(dirname(__DIR__).'/.env')) {
+        (new Dotenv())->load(dirname(__DIR__).'/.env');
+    }
+    $env = getenv('APP_ENV') ?: 'dev';
 }
 
-return function (array $context) use ($env, $debug) {
-    return new Kernel($env, $debug);
-};
+// Mode debug activé sauf si APP_ENV=prod
+$debug = (bool) (getenv('APP_DEBUG') ?: ('prod' !== $env));
+
+if ($debug) {
+    umask(0000);
+    Symfony\Component\ErrorHandler\Debug::enable();
+}
+
+// Configuration des proxys et hôtes de confiance
+if ($trustedProxies = getenv('TRUSTED_PROXIES')) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = getenv('TRUSTED_HOSTS')) {
+    Request::setTrustedHosts(explode(',', $trustedHosts));
+}
+
+// Initialisation du Kernel
+$kernel = new Kernel($env, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
